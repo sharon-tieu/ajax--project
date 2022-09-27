9aea8965fd15c07e770c43165345cf98b0d16a67
sharon-tieu <tieusharon@gmail.com>
2022-09-27 16:31:26 +0000

    xhr function in progress.

--- js/data.js
+++ js/data.js
@@ -1,5 +1,17 @@
 /* exported data */
-// var likesData = {
-//   entries: [],
-//   likesId: 1
-// };
+
+var data = {
+  view: 'home-view',
+  entries: [],
+  likesId: 1
+};
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  this.localStorage.setItem('user-likes-local-storage', dataJSON);
+});
+
+var previousDataJSON = localStorage.getItem('user-likes-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,34 +1,53 @@
+var $homeNavBar = document.querySelector('.home-nav-bar');
+var $searchNavBar = document.querySelector('.search-nav-bar');
+var $homeView = document.querySelector('.home-page-view');
+var $searchView = document.querySelector('.search-page-view');
+
+// VIEW SWAPPING
+function viewSwap(view) {
+  if (data.view === 'home-view') {
+    data.view = 'home-view';
+    $homeView.className = 'row home-view';
+    $searchView.className = 'hidden';
+  } else {
+    data.view = 'search-view';
+    $homeView.className = 'hidden';
+    $searchView.className = 'row search-view';
+  }
+}
+
+$searchNavBar.addEventListener('click', function () {
+  data.view = 'search-view';
+  viewSwap();
+});
+
+$homeNavBar.addEventListener('click', function () {
+  data.view = 'home-view';
+  viewSwap();
+});
+
+// HTTP REQUEST
+window.addEventListener('DOMContentLoaded', function (event) {
+  viewSwap();
+});
 
 var $studioGhibliInfo = document.querySelector('#card-box');
 
-function getInfo() {
+function getGhibli(id) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://ghibliapi.herokuapp.com/locations');
-  xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     // console.log(xhr.status);
     // console.log(xhr.response);
     for (var i = 0; i < this.responseType.length; i++) {
       var $li = document.createElement('li');
-      $li.textContent = xhr.response[i];
+      $li.textContent = xhr.response.name;
       $studioGhibliInfo.appendChild($li);
     }
   });
   xhr.send();
 }
 
-getInfo();
+getGhibli('Irontown');
 
-/*
-  <h4 class="font-comfortaa text-align-c">Card Title</h4>
-  <div class="divider-line"></div>
-  <p class="font-comfortaa padding-left-20 padding-right-20">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-*/
-
-var $cardTitle = document.createElement('h4');
-$cardTitle.setAttribute('class', 'font-comfortaa text-align-c');
+// var getLocation = ()
