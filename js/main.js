4915fb1f53c072e734d3bbfbb448503774d635a5
sharon-tieu <tieusharon@gmail.com>
2022-09-28 19:16:03 +0000

    Search results working. Fixing media queries.

--- js/main.js
+++ js/main.js
@@ -33,13 +33,12 @@ window.addEventListener('DOMContentLoaded', function (event) {
 // ====== HTTP REQUEST FUNCTION ====== //
 var $getGhibli = document.querySelector('.get-ghibli');
 var $cardLabel = document.querySelector('#card-label-result');
-var $cardInfoResult = document.querySelector('#card-info-result');
-var $cardPhoto = document.querySelector('#card-photo-result');
-var $allUlElements = document.querySelectorAll('ul');
+var $ulMovieTitle = document.querySelector('#movie-title');
+var $ulElements = document.querySelector('ul');
 
 function getGhibliCharacter(name) {
 
-  var $ulTitle = document.createElement('li');
+  var $ulTitle = document.createElement('p');
   var $liAge = document.createElement('li');
   var $liGender = document.createElement('li');
   var $liEyeColor = document.createElement('li');
@@ -60,28 +59,28 @@ function getGhibliCharacter(name) {
 
     // var $ulTitle = document.createElement('ul');
     $ulTitle.textContent = character[0].name;
-    $ulTitle.setAttribute('class', 'font-comfortaa search-item');
+    $ulTitle.setAttribute('class', 'font-comfortaa search-item text-align-center');
     $cardLabel.appendChild($ulTitle);
 
     // var $liAge = document.createElement('li');
     $liAge.textContent = 'Age: ' + character[0].age;
-    $liAge.setAttribute('class', 'font-comfortaa search-item');
-    $cardInfoResult.appendChild($liAge);
+    $liAge.setAttribute('class', 'font-comfortaa search-item text-align-left');
+    $cardLabel.appendChild($liAge);
 
     // var $liGender = document.createElement('li');
     $liGender.textContent = 'Gender: ' + character[0].gender;
-    $liGender.setAttribute('class', 'font-comfortaa search-item');
-    $cardInfoResult.appendChild($liGender);
+    $liGender.setAttribute('class', 'font-comfortaa search-item text-align-left');
+    $cardLabel.appendChild($liGender);
 
     // var $liEyeColor = document.createElement('li');
     $liEyeColor.textContent = 'Eye Color: ' + character[0].eye_color;
-    $liEyeColor.setAttribute('class', 'font-comfortaa search-item');
-    $cardInfoResult.appendChild($liEyeColor);
+    $liEyeColor.setAttribute('class', 'font-comfortaa search-item text-align-left');
+    $cardLabel.appendChild($liEyeColor);
 
     // var $liHairColor = document.createElement('li');
     $liHairColor.textContent = 'Hair Color: ' + character[0].hair_color;
-    $liHairColor.setAttribute('class', 'font-comfortaa search-item');
-    $cardInfoResult.appendChild($liHairColor);
+    $liHairColor.setAttribute('class', 'font-comfortaa search-item text-align-left');
+    $cardLabel.appendChild($liHairColor);
 
     // console.log('character[0].films[0]:', character[0].films[0]);
     var xhr2 = new XMLHttpRequest();
@@ -92,9 +91,10 @@ function getGhibliCharacter(name) {
       var $image = document.createElement('img');
       $image.setAttribute('src', xhr2.response.image);
       $image.setAttribute('class', 'movie-title-image search-item');
-      $cardPhoto.appendChild($image);
+      $ulMovieTitle.appendChild($image);
     });
     xhr2.send();
+    $ulMovieTitle.textContent = '';
 
     var xhr3 = new XMLHttpRequest();
     xhr3.open('GET', character[0].species);
@@ -103,15 +103,14 @@ function getGhibliCharacter(name) {
       // console.log('xhr3.response:', xhr3.response);
       var $liSpecies = document.createElement('li');
       $liSpecies.textContent = 'Species: ' + xhr3.response.name;
-      $liSpecies.setAttribute('class', 'font-comfortaa search-item');
-      $cardInfoResult.appendChild($liSpecies);
+      $liSpecies.setAttribute('class', 'font-comfortaa search-item text-align-left');
+      $cardLabel.appendChild($liSpecies);
     });
     xhr3.send();
   });
   xhr.send();
   $getGhibli.reset();
-  // console.log('event.target.closest(".search-item"):', event.target.closest('.search-item'));
-  $allUlElements.textContent = '';
+  $ulElements.textContent = '';
 }
 // getGhibliCharacter();
 
@@ -120,11 +119,4 @@ var $searchButton = document.querySelector('.search-btn');
 var $searchInfo = document.querySelector('.search-info');
 $searchButton.addEventListener('click', function () {
   getGhibliCharacter($searchInfo.value);
-  // data.oldSearches = $searchInfo.value;
-  // console.log('data.oldSearches:', data.oldSearches);
-  // console.log('event.target:', event.target);
-  // console.log('~event.target.tagName:', event.target.tagName);
-  // if (event.target.tagName === 'BUTTON') {
-  //   // console.log('event.target.closest(".search-item"):', event.target.closest('.search-item'));
-  // }
 });
