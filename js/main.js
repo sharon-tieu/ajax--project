b79a4ec3d9b5847c6b60c12c150b17fdec8c8312
sharon-tieu <tieusharon@gmail.com>
2022-12-06 21:56:36 +0000

    Changed everything to es6 arrow functions.

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ $imgChick.addEventListener('click', () => {
   viewSwap();
 });
 
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener('DOMContentLoaded', event => {
   // var $viewLikes = document.querySelector('.view-likes-list');
   if (data.likes.length > 0) {
     for (var i = 0; i < data.likes.length; i++) {
@@ -90,7 +90,7 @@ const getGhibliCharacter = name => {
   // $ulTitle.setAttribsucute('id', name);
 
   const targetUrl = encodeURIComponent('https://ghibli-api.sharonproject.com/people/');
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   // xhr.open('GET', 'https://ghibliapi.herokuapp.com/people/');
   xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
   xhr.responseType = 'json';
@@ -98,16 +98,16 @@ const getGhibliCharacter = name => {
     // console.log('xhr status:', xhr.status);
     // console.log('xhr response:', xhr.response);
 
-    var character = xhr.response.find(function (el) {
+    const character = xhr.response.find(el => {
       return el.name === name;
     });
     // console.log('character object:', character);
 
-    var $parentDiv = document.createElement('div');
+    const $parentDiv = document.createElement('div');
     $parentDiv.setAttribute('class', 'row align-items-center jc-flex-end');
     $cardLabel.appendChild($parentDiv);
 
-    var $colDiv = document.createElement('div');
+    const $colDiv = document.createElement('div');
     $colDiv.setAttribute('class', 'column-half');
     $parentDiv.appendChild($colDiv);
 
@@ -116,20 +116,20 @@ const getGhibliCharacter = name => {
     $ulTitle.setAttribute('class', 'font-comfortaa search-item text-align-center');
     $colDiv.appendChild($ulTitle);
 
-    var $colThird = document.createElement('div');
+    const $colThird = document.createElement('div');
     $colThird.setAttribute('class', 'column-one-third');
     $parentDiv.appendChild($colThird);
 
-    var $heart = document.createElement('i');
+    let $heart = document.createElement('i');
     $heart.setAttribute('class', 'fa-regular fa-heart');
     $colThird.appendChild($heart);
 
-    var $unHeart = document.createElement('i');
+    let $unHeart = document.createElement('i');
     $unHeart.setAttribute('class', 'fa-solid fa-heart pink-heart hidden');
     $colThird.appendChild($unHeart);
 
     $heart = document.querySelector('.fa-regular');
-    $heart.addEventListener('click', function () {
+    $heart.addEventListener('click', () => {
       // console.log('liked !');
       $heart.classList.add('hidden');
       $unHeart.classList.remove('hidden');
@@ -140,18 +140,18 @@ const getGhibliCharacter = name => {
     });
 
     $unHeart = document.querySelector('.fa-solid');
-    $unHeart.addEventListener('click', function () {
+    $unHeart.addEventListener('click', () => {
       // console.log('unliked !');
       $heart.classList.remove('hidden');
       $unHeart.classList.add('hidden');
     });
 
-    function userLikes() {
+    const userLikes = () => {
       var dataLikes = data.likes;
       if ($heart) {
         dataLikes.push(character);
       }
-    }
+    };
 
     // var $liAge = document.createElement('li');
     $liAge.textContent = 'Age: ' + character.age;
@@ -174,12 +174,12 @@ const getGhibliCharacter = name => {
     $cardLabel.appendChild($liHairColor);
 
     // console.log('character[0].films[0]:', character[0].films[0]);
-    var xhr2 = new XMLHttpRequest();
+    const xhr2 = new XMLHttpRequest();
     xhr2.open('GET', character.films[0]);
     xhr2.responseType = 'json';
-    xhr2.addEventListener('load', function () {
+    xhr2.addEventListener('load', () => {
       // console.log('xhr2.response.image: ', xhr2.response.image);
-      var $image = document.createElement('img');
+      const $image = document.createElement('img');
       $image.setAttribute('src', xhr2.response.image);
       $image.setAttribute('class', 'movie-title-image search-item');
       $ulMovieTitle.appendChild($image);
@@ -187,12 +187,12 @@ const getGhibliCharacter = name => {
     xhr2.send();
     $ulMovieTitle.textContent = '';
 
-    var xhr3 = new XMLHttpRequest();
+    const xhr3 = new XMLHttpRequest();
     xhr3.open('GET', character.species);
     xhr3.responseType = 'json';
-    xhr3.addEventListener('load', function () {
+    xhr3.addEventListener('load', () => {
       // console.log('xhr3.response:', xhr3.response);
-      var $liSpecies = document.createElement('li');
+      const $liSpecies = document.createElement('li');
       $liSpecies.textContent = 'Species: ' + xhr3.response.name;
       $liSpecies.setAttribute('class', 'font-comfortaa search-item text-align-left');
       $cardLabel.appendChild($liSpecies);
@@ -206,15 +206,15 @@ const getGhibliCharacter = name => {
 // getGhibliCharacter();
 
 // ====== SEARCH BUTTON ====== //
-var $searchButton = document.querySelector('.search-btn');
-var $searchInfo = document.querySelector('.search-info');
-$searchButton.addEventListener('click', function () {
+const $searchButton = document.querySelector('.search-btn');
+const $searchInfo = document.querySelector('.search-info');
+$searchButton.addEventListener('click', () => {
   getGhibliCharacter($searchInfo.value);
 });
 
-var $noLikesView = document.querySelector('.no-likes-view');
-var $likesListResults = document.querySelector('.likes-list-results');
-function noLikesView() {
+const $noLikesView = document.querySelector('.no-likes-view');
+const $likesListResults = document.querySelector('.likes-list-results');
+const noLikesView = () => {
   if (data.likes.length === 0) {
     $noLikesView.className = 'no-likes-view column-full margin-top-5 text-align-c padding-top-20';
     $likesListResults.className = 'hidden';
@@ -222,47 +222,47 @@ function noLikesView() {
     $noLikesView.className = 'hidden';
     $likesListResults.className = 'likes-list-results column-full margin-top-5 display-flex';
   }
-}
+};
 
 // LIKES LIST
-var $viewLikesList = document.querySelector('.view-likes-list');
+const $viewLikesList = document.querySelector('.view-likes-list');
 
-function showLikeEntry(likesEntry) {
+const showLikeEntry = likesEntry => {
 
-  var $divParentLayout = document.createElement('div');
+  const $divParentLayout = document.createElement('div');
   $divParentLayout.setAttribute('class', 'column-one-third display-flex jc-center align-items-center padding-top-10 padding-bottom-10');
   $viewLikesList.appendChild($divParentLayout);
 
-  var $cardBox = document.createElement('div');
+  const $cardBox = document.createElement('div');
   $cardBox.setAttribute('class', 'card-home-box');
   $divParentLayout.appendChild($cardBox);
 
-  var $contentParentDiv = document.createElement('div');
+  const $contentParentDiv = document.createElement('div');
   $contentParentDiv.setAttribute('class', 'text-align-c padding-top-10 display-flex align-items-center');
   $cardBox.appendChild($contentParentDiv);
 
-  var $pName = document.createElement('p');
+  const $pName = document.createElement('p');
   $pName.setAttribute('class', 'likes-data font-comfortaa column-half jc-flex-end text-align-right');
   $pName.textContent = likesEntry.name;
   $contentParentDiv.appendChild($pName);
 
-  var $heart = document.createElement('i');
+  const $heart = document.createElement('i');
   $heart.setAttribute('class', 'fa-solid fa-heart column-half pink-heart');
   $heart.setAttribute('id', likesEntry.id);
   $contentParentDiv.appendChild($heart);
 
-  $heart.addEventListener('click', function () {
-    var $modalContainer = document.querySelector('.modal-container');
+  $heart.addEventListener('click', () => {
+    const $modalContainer = document.querySelector('.modal-container');
     $modalContainer.classList.remove('hidden');
 
-    var $cancelButton = document.querySelector('.modal-cancel');
-    $cancelButton.addEventListener('click', function () {
+    const $cancelButton = document.querySelector('.modal-cancel');
+    $cancelButton.addEventListener('click', () => {
       $modalContainer.classList.add('hidden');
     });
 
-    var $removeButton = document.querySelector('.modal-remove');
+    const $removeButton = document.querySelector('.modal-remove');
 
-    $removeButton.addEventListener('click', function () {
+    $removeButton.addEventListener('click', () => {
       // debugger;
       $modalContainer.classList.add('hidden');
       for (var i = 0; i < data.likes.length; i++) {
@@ -289,39 +289,39 @@ function showLikeEntry(likesEntry) {
   //   }
   // });
 
-  var $ulData = document.createElement('ul');
+  const $ulData = document.createElement('ul');
   $ulData.setAttribute('class', 'text-align-left padding-top-10');
   $cardBox.appendChild($ulData);
 
-  var $liAge = document.createElement('li');
+  const $liAge = document.createElement('li');
   $liAge.setAttribute('class', 'font-comfortaa');
   $liAge.textContent = 'Age: ' + likesEntry.age;
   $ulData.appendChild($liAge);
 
-  var $liGender = document.createElement('li');
+  const $liGender = document.createElement('li');
   $liGender.textContent = 'Gender: ' + likesEntry.gender;
   $liGender.setAttribute('class', 'font-comfortaa');
   $ulData.appendChild($liGender);
 
-  var $liEyeColor = document.createElement('li');
+  const $liEyeColor = document.createElement('li');
   $liEyeColor.textContent = 'Eye Color: ' + likesEntry.eye_color;
   $liEyeColor.setAttribute('class', 'font-comfortaa');
   $ulData.appendChild($liEyeColor);
 
-  var $liHairColor = document.createElement('li');
+  const $liHairColor = document.createElement('li');
   $liHairColor.textContent = 'Hair Color: ' + likesEntry.hair_color;
   $liHairColor.setAttribute('class', 'font-comfortaa');
   $ulData.appendChild($liHairColor);
 
-  var likesSpecies = new XMLHttpRequest();
+  const likesSpecies = new XMLHttpRequest();
   likesSpecies.open('GET', likesEntry.species);
   likesSpecies.responseType = 'json';
   likesSpecies.addEventListener('load', function () {
-    var $liSpecies = document.createElement('li');
+    const $liSpecies = document.createElement('li');
     $liSpecies.textContent = 'Species: ' + likesSpecies.response.name;
     $liSpecies.setAttribute('class', 'font-comfortaa');
     $ulData.appendChild($liSpecies);
   });
 
   likesSpecies.send();
-}
+};
